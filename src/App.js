138b52f5dfb5360b0c7142dc9aa790857eb7193b
jaderyan <jadeelizabethryan@outlook.com>
2020-02-06 13:44:16 +0000

    link component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import Button from "./components/Button/Button";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import SignUpButton from "./components/SignUpButton/SignUpButton";
+import Link from "./components/Link/Link";
+import { BrowserRouter } from "react-router-dom";
 
 library.add(fas);
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Button text="Read more" />
-        <Button text="Read more" disabled={true} />
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Button text="Read more" />
+          <Button text="Read more" disabled={true} />
 
-        <Button text="Read more" alt={true} />
-        <Button text="Read more" alt={true} disabled={true} />
+          <Button text="Read more" alt={true} />
+          <Button text="Read more" alt={true} disabled={true} />
 
-        <SignUpButton text="Sign up" />
-        <SignUpButton text="Sign up" disabled={true} />
+          <SignUpButton text="Sign up" />
+          <SignUpButton text="Sign up" disabled={true} />
+        </div>
+
+        <div style={{ padding: "20px 0" }}>
+          <Link to="/" text="Read more" />
+          <Link to="/" text="Read more" icon={true} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
+import read from "../../assets/icons/read.svg";
 
-const Link = ({ path, text }) => <RouterLink to={path}>{text}</RouterLink>;
+import "./Link.scss";
+import { ReactSVG } from "react-svg";
+
+const Link = ({ path, text, icon }) => (
+  <RouterLink to={path} className="link">
+    <div className="link--container">
+      <span className="link--text">{text}</span>
+      {icon && <ReactSVG src={read} className="link--icon" />}
+    </div>
+  </RouterLink>
+);
 
 export default Link;
